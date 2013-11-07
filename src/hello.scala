54cd4027893e17a7559769c0e37e84f1d290baa9
vasily-knk <vasily.knk@gmail.com>
2013-11-08 00:13:36 +0400

    String literals

--- src/hello.scala
+++ src/hello.scala
@@ -2,40 +2,56 @@ import scala.util.parsing.combinator._
 import scala.collection.immutable.HashMap
 
 object Arith {
-    type Context = Map[String, Double]
+    trait Value {
+        def value: Double
+        def literal: String
+        case class WrongType(smth:String)  extends Exception
+    }
+
+    case class NumValue(value: Double) extends Value {
+        def literal: String = throw new WrongType("Double -> String")
+    }
+
+    case class LitValue(literal: String) extends Value {
+        def value: Double = throw new WrongType("String -> Double")
+    }
+
+    type Context = Map[String, Value]
 
     trait Expr {
-        def eval(context: Context) : Double
+        def eval(context: Context) : Value
     }
 
     case class BinaryExpression(left: Expr, op: String, right: Expr) extends Expr {
-        def eval(context: Context) : Double = {
+        def eval(context: Context) : Value = {
             op match {
-                case "+" => left.eval(context) + right.eval(context)
-                case "-" => left.eval(context) - right.eval(context)
-                case "*" => left.eval(context) * right.eval(context)
-                case "/" => left.eval(context) / right.eval(context)
+                case "+" => NumValue(left.eval(context).value + right.eval(context).value)
+                case "-" => NumValue(left.eval(context).value - right.eval(context).value)
+                case "*" => NumValue(left.eval(context).value * right.eval(context).value)
+                case "/" => NumValue(left.eval(context).value / right.eval(context).value)
             }
         }
     }
 
-    case class LiteralExpression(number: Double) extends Expr {
-        def eval(context: Context): Double = number
+    case class NumericExpression(number: Double) extends Expr {
+        def eval(context: Context): Value = NumValue(number)
+    }
+
+    case class LiteralExpression(string: String) extends Expr {
+        def eval(context: Context): Value = LitValue(string)
     }
 
     case class ParenthesisedExpression(expr: Expr) extends Expr {
-        def eval(context: Context): Double = expr.eval(context)
+        def eval(context: Context): Value = expr.eval(context)
     }
 
     case class VariableExpression(name: String) extends Expr {
-        def eval(context: Context): Double = {
+        def eval(context: Context): Value = {
             context.get(name).get
         }
     }
 
-    case class Assignment(name: String, e: Expr) {
-
-    }
+    case class Assignment(name: String, e: Expr)
 }
 
 class Arith extends JavaTokenParsers {
@@ -47,32 +63,36 @@ class Arith extends JavaTokenParsers {
                 case (res, op ~ right) => BinaryExpression(res, op, right)
             }
     }
-    def foo: PartialFunction[ ~[String, Expr], Assignment] = {
-        case name ~ e => Assignment(name, e)
+    def foo(name: String, e: Expr) : Assignment = {
+        Assignment(name, e)
     }
 
-    def parseBody(list : List[Assignment]) : Context = {
-        var context : Context = new HashMap[String, Double]
+    def parseBody(list: List[Assignment], expr: Expr) : Value = {
+        var context : Context = new HashMap[String, Value]
         list.map({
             case Assignment(name, e) => context = context + (name -> e.eval(context))
         })
-        context
+        expr.eval(context)
     }
 
     def expr: Parser[Expr] = term ~ rep("+" ~ term | "-" ~ term) ^^ convertToBinary
     def term: Parser[Expr] = factor ~ rep("*" ~ factor | "/" ~ factor) ^^ convertToBinary
-    def factor: Parser[Expr] =
-        floatingPointNumber ^^ { case s => LiteralExpression(s.toDouble) } |
-        ident               ^^ { case s => VariableExpression(s)} |
-        ("(" ~> expr <~ literal(")"))
 
-    def assign: Parser[Assignment] = (ident <~ "=") ~ expr ^^ foo
-    def body: Parser[Context] = repsep(assign, ";") ^^ parseBody
+    def factor: Parser[Expr] =
+        floatingPointNumber ^^ { case s => NumericExpression(s.toDouble) } |
+        ident               ^^ { case s => VariableExpression(s)} |
+        ("(" ~> expr <~ ")")
+
+    def assign: Parser[Assignment] = (ident <~ "=") ~ expr ^^ { case name ~ e => Assignment(name, e) }
+
+    def body: Parser[Value] = (repsep(assign, ";") <~ ";") ~ expr ^^ { case list ~ e => parseBody(list, e) }
 }
 
 object Hello extends Arith {
+    def sum(a:Int, b:Int) = a + b
+
     def main(args: Array[String]) {
-        println(parseAll(body, "a = 7 + 8; b = 5 + a"))
+        println(parseAll(body, "a = 7 + 8; a = 5 + a; a * 2"))
     }
 }
 
